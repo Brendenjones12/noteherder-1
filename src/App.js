2d6c2124934d660f1db5aaff5bb56ceeab534d33
Brenden Jones <brendenjones4320@gmail.com>
2017-07-10 10:08:36 -0400

    I added it to firebase and set up all the authentication

--- src/App.js
+++ src/App.js
@@ -8,39 +8,65 @@ class App extends Component {
     super()
 
     this.state = {
-      notes: {
-        'note-1': {
-          id: 'note-1',
-          title: 'My fancy note from App',
-          body: 'This note is so fancy!',
-        },
-        'note-2': {
-          id: 'note-2',
-          title: 'Another one from App',
-          body: 'Also very fancy',
-        },
-      },
-      currentNote: {
-        id: null,
-        title: ' ',
-        body: ' '
-      },
+      notes: {},
+      currentNote: this.blankNote(),
     }
   }
 
-setCurrentNote(note) {
-this.setState({currentNote: note})
-}
+  blankNote = () => {
+    return {
+      id: null,
+      title: '',
+      body: '',
+    }
+  }
 
+  setCurrentNote = (note) => {
+    this.setState({ currentNote: note })
+  }
 
+  resetCurrentNote = () => {
+    this.setCurrentNote(this.blankNote())
+  }
+
+  saveNote = (note) => {
+    const notes = {...this.state.notes}
+    if (!note.id) {
+      note.id = Date.now()
+    }
+    notes[note.id] = note
+
+    this.setState({ notes })
+    this.setCurrentNote(note)
+  }
+
+  removeCurrentNote = () => {
+    const notes = {...this.state.notes}
+    delete notes[this.state.currentNote.id]
+
+    this.setState({ notes })
+    this.resetCurrentNote()
+  }
 
   render() {
+    const actions = {
+      setCurrentNote: this.setCurrentNote,
+      resetCurrentNote: this.resetCurrentNote,
+      saveNote: this.saveNote,
+      removeCurrentNote: this.removeCurrentNote,
+    }
+
+    const noteData = {
+      notes: this.state.notes,
+      currentNote: this.state.currentNote,
+    }
+
     return (
       <div className="App">
-        <Main 
-        notes={this.state.notes}
-        currentNote={this.state.currentNote} 
-        setCurrentNote={this.setCurrentNote.bind(this)}/>
+        <Main
+          {...actions}
+          {...noteData}
+        />
       </div>
     );
   }
